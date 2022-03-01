1bcde078d43437e14fd22739a26c41730360ebf9
samiullahbahadur <bahadursamiullah@gmail.com>
2022-03-01 20:27:52 +0430

    Add stringlength

---
+++ stringLength.js
@@ -0,0 +1,9 @@
+function stringLength(string) {
+  if (string.length >= 1 && string.length < 10) {
+    return string.length;
+  } else return false;
+}
+stringLength("h1");
+stringLength("");
+stringLength("Hello World");
+module.exports = stringLength;
